913c3b9ba6d3326403d636398e821b8b938eeeb1
Carina Viana <carina.viana@etec.sp.gov.br>
2021-05-24 18:07:33 -0300

    Adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from './../models/IFilmes.model';
 import { Component } from '@angular/core';
 import { AlertController, SelectValueAccessor } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,55 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mulher-Maravilha 1984',
+      lancamento: '17/12/2020',
+      duracao: '2h 31m',
+      classificacao: 67,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qDA95ebiy3W3m8hTRB3xZNZVVBM.jpg',
+      generos: ['Fantasia', 'Ação', 'Aventura']
+    },
+
+    {
+      nome: 'Tom & Jerry - O Filme(2021)',
+      lancamento: '11/02/2021',
+      duracao: '1h 41m',
+      classificacao: 73,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9NvYyM8H6d5KAVGqpyFV9YPO5cU.jpg',
+      generos: ['Comédia', 'Família', 'Animação']
+    },
+
+    {
+      nome: 'Raia e o Último Dragão',
+      lancamento: '04/03/2021',
+      duracao: '1h 47m',
+      classificacao: 82,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/o2NTWpD6LVf1YyPKTdvcEuHqcJ6.jpg',
+      generos: ['Aventura', 'Animação', 'Fantasia']
+      },
+
+      {
+        nome: 'Pets Monstruosos',
+        lancamento: '02/04/2021',
+        duracao: '1h 47m',
+        classificacao: 77,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/dkokENeY5Ka30BFgWAqk14mbnGs.jpg',
+        generos: ['Aventura', 'Animação', 'Fantasia']
+        },
+
+        {
+          nome: 'Soul: Uma Aventura com Alma ',
+          lancamento: '25/12/2020',
+          duracao: '1h 40m',
+          classificacao: 83,
+          cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bzDAfXoqNAvWUe7uss2NE3BmRqy.jpg',
+          generos: ['Aventura', 'Animação', 'Fantasia']
+        }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
