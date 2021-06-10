16c3569f005f164186871d9dd823adb985675ca9
Carina Viana <carina.viana@etec.sp.gov.br>
2021-06-10 10:39:42 -0300

    correção nos dados da série

--- src/app/dados-series/dados-series.page.ts
+++ src/app/dados-series/dados-series.page.ts
@@ -26,9 +26,9 @@ export class DadosSeriesPage implements OnInit {
     this.series = this.dadosService.pegarDados('serie');
     this.generos = this.dadosService.pegarDados('generos');
 
-    this.generoService.buscarGenero('serie').subscribe(dados =>{
+    this.generoService.buscarGenero('tv').subscribe(dados =>{
       dados.genres.forEach(genero => {
-        this.generoService[genero.id] = genero.name;
+        this.generos[genero.id] = genero.name;
       });
     });
   }
